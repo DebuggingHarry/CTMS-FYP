335d8c1cf7c30c3c7e114e2d7f92006b770b6a0b
Harry <97326280+DebuggingHarry@users.noreply.github.com>
2025-11-14 21:05:14 +0000

    Added Middleware and posting capabilities

--- app.js
+++ app.js
@@ -7,19 +7,15 @@ import database from "./database.js";
 const app = express();
 
 // Middleware ----------------------------------------------
-app.use(express.json());
-
+app.use(express.json({ type: "*/*" }));
 const ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:3000";
-
 app.use(
   cors({
     origin: ORIGIN,
     credentials: true,
   })
 );
-
 app.options(/.*/, cors({ origin: ORIGIN, credentials: true }));
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", ORIGIN);
   res.header("Access-Control-Allow-Credentials", "true");
@@ -31,34 +27,92 @@ app.use((req, res, next) => {
   if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
 });
+app.use(express.json({ type: "*/*" }));
+app.use(express.urlencoded({ extended: true }));
 
 // Controllers ---------------------------------------------
 
-const buildTrialsSelectSQL = (id, variant) => {
-  let sql = "";
+const buildSetFields = (fields) =>
+  fields.reduce(
+    (setSQL, field, index) =>
+      setSQL + `${field}=:${field}` + (index === fields.length - 1 ? "" : ", "),
+    `SET `
+  );
+const buildTrialsInsertSQL = (record = {}) => {
+  const table = `clinicaltrials`;
+  let mutableFields = ["trial_name"];
+  const name = record.trial_name;
 
-  let table = `
+  return `INSERT INTO ${table} ` + buildSetFields(mutableFields);
+};
+
+const buildTrialsSelectSQL = (id, variant) => {
+  const table = `clinicaltrials AS ct`;
+  const extendedTable = `
   clinicaltrials AS ct
   LEFT JOIN crctrials AS crc
     ON crc.trial_id = ct.trial_id
 `;
 
-  let fields = `
+  const fields = `
   ct.trial_id,
   ct.trial_name
 `;
+  const extendedFields = fields;
+
+  let sql = "";
 
   switch (variant) {
     default:
       sql = `
     SELECT
-      ${fields} FROM
+      ${fields}
+    FROM
       ${table}
+    ORDER BY
+      ct.trial_name ASC;
+    `;
+      break;
+
+    case "clinical":
+      sql = `
+    SELECT
+      ${extendedFields}
+    FROM
+      ${extendedTable}
     `;
       if (id) sql += `WHERE crc.user_id = ${id}`;
+      sql += `
+    ORDER BY
+      ct.trial_name ASC;
+    `;
   }
 
   return sql;
+};
+
+const createTrials = async (sql, record) => {
+  try {
+    const status = await database.query(sql, record);
+
+    const recoverRecord = buildTrialsSelectSQL(status[0].insertId, null);
+
+    const { isSuccess, result, message } = await read(recoverRecord);
+
+    return isSuccess
+      ? { isSuccess: true, message: "Record retrieved successfully.", result }
+      : {
+          isSuccess: false,
+          message: `Failed to recover the inserted record: ${message}`,
+          result: null,
+        };
+  } catch (error) {
+    return {
+      isSuccess: false,
+      message: `Failed to execute query: ${error.message}`,
+      result: null,
+    };
+  }
 };
 
 const read = async (sql) => {
@@ -76,16 +130,7 @@ const read = async (sql) => {
   }
 };
 
-const fromDbTrialRow = (r) => ({
-  trialId: Number(r.trial_id),
-  trialName: r.trial_name,
-});
-
-const fromDbTrials = (rows) => rows.map(fromDbTrialRow);
-
-const getTrialsController = async (req, res, variant) => {
-  const id = Number(req.params.crc_id);
-
+const getTrialsController = async (res, id, variant) => {
   const sql = buildTrialsSelectSQL(id, variant);
 
   const { isSuccess, message, result } = await read(sql);
@@ -95,10 +140,23 @@ const getTrialsController = async (req, res, variant) => {
   res.status(200).json(result);
 };
 
+const postTrialsController = async (req, res) => {
+  const sql = buildTrialsInsertSQL(req.body);
+
+  const { isSuccess, message, result } = await createTrials(sql, req.body);
+  if (!isSuccess) {
+    return res.status(500).json({ message });
+  }
+  res.status(201).json(result);
+};
+
 // Endpoints -----------------------------------------------
 app.get("/api/trials/crc/:crc_id", (req, res) =>
-  getTrialsController(req, res, null)
+  getTrialsController(res, req.params.crc_id, "clinical")
 );
+app.get("/api/trials", (req, res) => getTrialsController(res, null, null));
+
+app.post("/api/trials", postTrialsController);
 
 // Start server --------------------------------------------
 const PORT = process.env.PORT || 5000;
